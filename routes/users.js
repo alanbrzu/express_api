c8f4d776f7aacbfe2a54a648f0eb38f0a564a8ae
Alan Brzuszko <alanbrzu@gmail.com>
2022-07-13 14:24:36 -0400

    api setup

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,37 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("user list");
+  res.send("please login");
 });
 
-router.get("/new", (req, res) => {
-  res.send("new form");
+router.get("/existing", (req, res) => {
+  res.send("welcome user");
+});
+
+router.get("/newUser", (req, res) => {
+  res.send("create account");
+});
+
+//need post to create account
+
+router
+  .route("/:id")
+  .get((req, res) => {
+    console.log(req.user.name);
+    res.send(`hello user ${req.params.id}`);
+  })
+  .put((req, res) => {
+    res.send(`Updated user ${req.params.id}`);
+  })
+  .delete((req, res) => {
+    res.send(`Deleted user ${req.params.id}`);
+  });
+
+const users = [{ name: "alan" }, { name: "brian" }];
+
+router.param("id", (req, res, next, id) => {
+  req.user = users[id];
+  next();
 });
 
 export default router;
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 import express from "express";
 import router from "./routes/users.js";
+
 const app = express();
+
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  console.log("howdy");
-  res.render("index", { text123: "world" });
-});
-
-const kek = "kek";
-
-const userRouter = router;
-
-app.use("/users", userRouter);
+app.use("/users", router);
 
 app.listen(3005);
